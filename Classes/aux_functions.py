c772f517c4f0f256245b3ecfaa5d5f50ce257bb3
rcampello <rafael.campello@hotmail.com>
2019-04-18 17:32:02 -0300

    Classes.

--- Classes/aux_functions.py
+++ Classes/aux_functions.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
 import re
+import chess
 
-def center_image(img, p = 0.35, boolean = True):
+def center_image(img, boolean = True, p = 0.35):
     if boolean == False:
         return img
     w, h = img.shape
@@ -10,26 +11,78 @@ def center_image(img, p = 0.35, boolean = True):
     #p = 0.4 #max 0.5
     return img[int(cw - w*p): int(cw + w*p), int(ch - h*p) : int(ch + h*p)]
 
+def square_color(i, j):
+    return ("Black" if (i + j) % 2 else "White")
+
+def test_f1(a, b, threshold = 5):
+    a1 = np.sum(cv2.Canny(a, 0, 25))/(255)
+    b1 = np.sum(cv2.Canny(b, 0, 25))/(255)
+    return (a1 > threshold, b1 > threshold)
+
+def detect_change(a, b, plot = False):
+    img1 = center_image(a, p = 0.25)
+    img2 = center_image(b, p = 0.25)
+    
+    if plot:
+        fig, axs = plt.subplots(1, 2, figsize = (10, 10))
+        axs[0].imshow(cv2.Canny(img1, 0, 25))
+        axs[1].imshow(cv2.Canny(img2, 0, 25))
+
+    return test_f1(img1, img2)
+
 def check_diff(a, b):
-    M = max(a, b)
-    return abs(a-b)/M
+    M, m = max(a, b), min(a, b)
+    return (m/M)
 
-def dist_imgs(a, b, bins = 256):
-    hista = cv2.calcHist([a],[0],None,[bins],[0,256]).ravel()
-    histb = cv2.calcHist([b],[0],None,[bins],[0,256]).ravel()
-    
-    hista_n = hista/np.linalg.norm(hista)
-    histb_n = histb/np.linalg.norm(histb)
-    
-    return 1 - check_diff(np.std(hista), np.std(histb))
-    #usando std apenas
-    return np.max(np.correlate(hista_n, histb_n, "full"))
 
-def matrix_to_chess_notation(array):
-    pos1, pos2 = array
-    l1, n1 = chr(97 + pos1[1]), 8 - pos1[0]
-    l2, n2 = chr(97 + pos2[1]), 8 - pos2[0]
-    return ("{}{}{}{}".format(l1, n1, l2, n2), "{}{}{}{}".format(l2, n2, l1, n1))
+def detect_move(array_geral, jogada):
+    matrix = np.zeros((8, 8), dtype = np.uint)
+    
+    for x in range(8):
+        for y in range(8):
+            img1 = center_image(array_geral[jogada][x, y], True)
+            img2 = center_image(array_geral[jogada+1][x, y], True)
+            detected = 1 if check_diff(np.std(img1), np.std(img2)) < 0.5 else 0
+#             if detected == 1:
+#                 detected = np.sum(detect_change(array_geral[jogada][x, y], array_geral[jogada + 1][x, y])) % 2
+            matrix[x, y] = detected
+    
+    #correct false positives
+    for pos in np.transpose(np.nonzero(matrix)):
+        if detect_change(array_geral[jogada][pos[0], pos[1]], array_geral[jogada+1][pos[0], pos[1]]) == (False, False):
+            matrix[pos[0], pos[1]] = 0
+    
+    if np.sum(matrix) == 2:
+        pass
+    elif np.sum(matrix) == 4:
+        roque_curto = np.array([1, 1, 1, 1])
+        roque_longo = np.array([1, 0, 1, 1, 1])
+        if np.array_equal(matrix[0][4:], roque_curto):
+            #print("Roque curto Pretas!")
+            matrix[0][4:] = np.array([1, 0, 1, 0])
+        elif np.array_equal(matrix[7][4:], roque_curto):
+            #print("Roque curto Brancas!")
+            matrix[7][4:] = np.array([1, 0, 1, 0])
+        elif np.array_equal(matrix[0][:5], roque_longo):
+            #print("Roque longo Pretas!")
+            matrix[0][:5] = np.array([0, 0, 1, 0, 1])
+        elif np.array_equal(matrix[7][:5], roque_longo):
+            #print("Roque longo Brancas!")
+            matrix[7][:5] = np.array([0, 0, 1, 0, 1])
+        else:
+            print("ERRO na identificação da jogada (sum == 4): {}".format(jogada))
+        #print("Roque na jogada: {}.".format(jogada))
+        pass
+    else:
+        print("ERRO na identificação da jogada: {}.".format(jogada))
+    
+    #correct false positives
+    for pos in np.transpose(np.nonzero(matrix)):
+        if detect_change(array_geral[jogada][pos[0], pos[1]], array_geral[jogada+1][pos[0], pos[1]]) == (False, True):
+            matrix[pos[0], pos[1]] = 2
+        elif detect_change(array_geral[jogada][pos[0], pos[1]], array_geral[jogada+1][pos[0], pos[1]]) == (True, True):
+            matrix[pos[0], pos[1]] = 2
+    return matrix
 
 def atoi(text):
     return int(text) if text.isdigit() else text
--- Classes/main.py
+++ Classes/main.py
@@ -7,160 +7,44 @@ import os
 from aux_functions import *
 
 class Game():
-    def __init__(self, size = (400, 400)):
-        self.aux = np.zeros(size, dtype = np.float32)
-        self.pts_aux_square = np.float32([[0,0],[self.aux.shape[1], 0],[self.aux.shape[1],self.aux.shape[0]],[0,self.aux.shape[0]]])
-
-        #self.board = chess.Board()
-
-        self.path_files(path = "../Jogos/1")
-        imgInicial = cv2.imread(self.files[0], 0)
-        self.calibration(imgInicial)
-
-        self.general_arrays()
-        self.movements()
-        self.jogo_ate_i()
+    def __init__(self, size = (400, 400), game_n = 1):
+        
+        #depois botar as funções que geram os dois arrays abaixo, por enquanto criar nos jupyters e dar load
+        self.load_array_geral("../Jogos/{}/array_geral_{}.npy".format(str(game_n), str(game_n)))
+        self.load_array_jogadas("../Jogos/{}/array_jogadas_{}.npy".format(str(game_n), str(game_n)))
         return
 
-    def calibration(self, img_calibration):
-        points = []
-        ix,iy = -1,-1
-        img = img_calibration
+    def load_array_geral(self, path):
+        self.array_geral = np.load(path)
 
-        def get_points(event,x,y,flags,param):
-            global ix, iy, drawing,mode
+    def load_array_jogadas(self, path):
+        self.array_jogadas = np.load(path)
 
-            if event == cv2.EVENT_LBUTTONDOWN:
-                ix,iy = x,y
-                #print(x, y)
-                points.append([x, y])
-
-        self.img1_r = cv2.resize(img, (int(img.shape[1]/1.4), int(img.shape[0]/1.4)))
-
-        cv2.imshow("click", self.img1_r)
-        cv2.setMouseCallback('click', get_points)
-
-        while(1):
-            k = cv2.waitKey(1) & 0xFF
-            if k == ord('q') or k == 27:
-                break
-                
-        cv2.destroyAllWindows()
-
-        self.pts1 = np.float32(points[:4])
-        if len(self.pts1) == 0:
-            self.pts1 = np.array([[481., 156.], [852., 158.], [852., 522.], [478., 523.]], dtype = np.float32)
-        return
-
-    def path_files(self, path):
-        self.files = []
-        for r, d, f in os.walk(path):
-            for file in f:
-                if '.jpg' in file:
-                    self.files.append(os.path.join(r, file))
-        self.files.sort(key=natural_keys)
-        return 
-
-    def general_arrays(self):
-        perspectiva = []
-        lst_geral = []
-
-        for path in self.files:
-            img = cv2.imread(path, 0)
-            M = cv2.getPerspectiveTransform(self.pts1, self.pts_aux_square)
-            img_r = cv2.resize(img, (int(img.shape[1]/1.4), int(img.shape[0]/1.4)))
-            perspectiva.append(cv2.warpPerspective(img_r, M,(self.aux.shape[1],self.aux.shape[0]), self.aux, borderMode = cv2.BORDER_TRANSPARENT))
-            lst = []
-            for i in range(8):
-                for j in range(8):
-                    lst.append(perspectiva[-1][perspectiva[-1].shape[1]*i//8:perspectiva[-1].shape[1]*(i+1)//8, perspectiva[-1].shape[0]*j//8:perspectiva[-1].shape[0]*(j+1)//8])
-            array = np.array(lst).reshape(8, 8, 50, -1)
-            lst_geral.append(array)
-
-        self.array_imgs = np.array(perspectiva)
-        self.array_geral = np.array(lst_geral)
-        return 
-
-    def movement(self, jogada, use_center = False, p = 0.35):
-        array_geral = self.array_geral
-
-        imgs = (jogada-1, jogada)
-        lst, two_low = [], []
-        
-    #     prev = center_image(array_geral[imgs[0]], p = p, boolean = use_center)
-    #     curr = center_image(array_geral[imgs[1]], p = p, boolean = use_center)
-        
+    def show_full_board(self, jogada = 0, center = False, p = 0.35):
+        fig, axs = plt.subplots(8, 8, figsize = (10, 10))
+        [axi.set_axis_off() for axi in axs.ravel()]
         for i in range(8):
             for j in range(8):
-                prev = center_image(array_geral[imgs[0]][i, j], p = p, boolean = use_center)
-                curr = center_image(array_geral[imgs[1]][i, j], p = p, boolean = use_center)
-                dist_imgs(prev, curr)
-                lst.append(dist_imgs(prev, curr))
-        lst = np.array(lst).reshape(8, 8)
-        
-        for i in range(2):
-            idx = np.argmin(lst, axis = None) 
-            ind = np.unravel_index(idx, lst.shape)
-
-            two_low.append(ind)
-        
-            lst[ind[0], ind[1]] = 1
-        return np.array(two_low)
-
-    def movements(self):
-        i = len(self.array_imgs)
-        jogadas = []
-        for i in range(1, i):
-            jogadas.append(self.movement(jogada = i, use_center = False, p = 0.45))
-        jogadas = np.array(jogadas)
-        self.lst_moves = np.array([matrix_to_chess_notation(x) for x in jogadas])
+                axs[i, j].imshow(center_image(self.array_geral[jogada][i, j], boolean = center, p = p))
+        plt.show()
         return
     
-    def debug_step(self, n, pos, center = False, p = 0.35, bins = 256):
-        fig, axs = plt.subplots(1, 3, figsize = (20, 6))
-
-        img1 = self.array_geral[n][pos[0], pos[1]]
-        img2 = self.array_geral[n+1][pos[0], pos[1]]
-
-        if center == True:
-            img1 = center_image(img1, p = p)
-            img2 = center_image(img2, p = p)
-
-        h1 = cv2.calcHist([img1],[0],None,[bins],[0,256]).ravel()
-        h2 = cv2.calcHist([img2],[0],None,[bins],[0,256]).ravel()
-
-
-        axs[0].imshow(img1)
-        axs[1].imshow(img2)
-
-        axs[2].plot(h1)
-        axs[2].plot(h2)
-
-        plt.show()
-        return(img1, img2, h1, h2, dist_imgs(img1, img2))
-
-    def push_board(self, move):
-        try:
-            self.board.push_uci(move[0])
-            return
-        except:
-            try:
-                self.board.push_uci(move[1])
-                return 
-            except:
-                print("\n\n\n\n\nErro!\n\n\n\n\n")
-                return -1
-    def jogo_ate_i(self, i = 9999):
-        self.board = chess.Board()
-        for jogada in self.lst_moves[:i]:
-            self.push_board(jogada)
+    def detect_move(self, jogada):
+        print(detect_move(self.array_geral, jogada))
         return
+
+    def jogo_ate_i(self, i = 9999, print = True):
+        self.board = chess.Board()
+        for jogada in self.array_jogadas[:i]:
+            self.board.push_uci(jogada)
+        print(self.board)
+        return self.board
+
     
 if __name__ == "__main__":
     c = Game()
-
-    print(c.board)
-    c.debug_step(0, (4, 3))[-1]
+    c.jogo_ate_i(2)
+ 
 
     #print("Img Similarity: ", c.debug_step(0, (4, 3))[-1])
 
